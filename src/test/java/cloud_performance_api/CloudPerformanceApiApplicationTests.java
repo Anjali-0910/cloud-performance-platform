8f5f8779005575de8076fcac37a4e8ac622fd4a8
aakcodez <anvitakattimani@gmail.com>
2026-09-14 17:28:59 +0530

    Initial Spring Boot application

---
+++ src/test/java/cloud_performance_api/CloudPerformanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud_performance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPerformanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
